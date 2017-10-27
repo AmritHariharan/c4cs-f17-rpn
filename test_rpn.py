137d74af2602d7f5ef7c9a9428ce7b6d99069fb2
AmritHariharan <stuka241@gmail.com>
2017-10-27 12:05:31 -0400

    added test

--- test_rpn.py
+++ test_rpn.py
@@ -1,2 +1,8 @@
 import unittest
 import rpn
+
+class TestBasics(unittest.TestCase):
+	
+	def test_add(self): # self is always the first argument
+		result = rpn.calculate('1 1 +')
+		self.assertEqual(2, result)
