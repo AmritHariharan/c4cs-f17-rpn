6428292fef9d04040fc118f3375a34956fb60dc6
AmritHariharan <stuka241@gmail.com>
2017-11-07 11:49:51 -0500

    added colours

--- rpn.py
+++ rpn.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python3
 
 import operator
+import readline
+from termcolor import colored
 
 ops = {
 	'+': operator.add,
 	'-': operator.sub,
 	'*': operator.mul,
+	'/': operator.truediv,
 }
 
+cols = {
+	'+': 'cyan',
+	'-': 'cyan',
+	'*': 'cyan',
+	'/': 'cyan',
+	'neg': 'red',
+	'pos': 'green',
+}
+
+def validCol(token):
+	try:
+		val = int(token)
+		if val < 0:
+			return 'neg'
+		else:
+			return 'pos'
+	except ValueError:
+		return token
+
 def calculate(string):
+
 	# create a list
 	stack = []
 
@@ -21,10 +44,11 @@ def calculate(string):
 			arg1 = stack.pop()
 			function = ops[token]
 			stack.append(function(arg1, arg2))
-			
+			print(*[colored(i, cols[validCol(i)]) for i in stack])
 	return stack.pop()
 
 def main():
+	print(colored('RPN CALCULATOR', 'red'))
 	while True:
 		calculate(input("rpn calc> "))
 
